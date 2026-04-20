1ec1af6aff65e27a1360fc8346241c7ce1c9c3b8
roggm <124746334+roggm@users.noreply.github.com>
2026-04-19 20:32:03 -0600

    commit 3

--- terraform/modules/d1/main.tf
+++ terraform/modules/d1/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
 resource "cloudflare_d1_database" "this" {
   account_id = var.account_id
   name       = var.database_name
